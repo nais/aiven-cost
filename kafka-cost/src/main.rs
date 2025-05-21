f910f5a5aab6bdc450061ed836d2d54b6e359d6b
carl hedgren <carl.hedgren@nav.no>
2025-05-21 13:30:23 +0200

    get billing stuff
    Actually maybe not what we need to get but this
    is a hand-sucked client, RIP
    
    Co-authored-by: x10an14-nav <christian.chavez@nav.no>

--- kafka-cost/src/main.rs
+++ kafka-cost/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tracing::info;
+use std::collections::HashMap;
+use serde_json::json;
+use reqwest::{header, ClientBuilder};
+
+pub fn init_tracing_subscriber() -> Result<()> {
+    tracing_subscriber::fmt()
+        .init();
+    Ok(())
+}
+
+
+fn client(token: &str) -> Result<reqwest::Client>  {
+    reqwest::Client::builder()
+    .https_only(true).user_agent("nais-kafka-cost")
+        .build().map_err(anyhow::Error::msg)
+
+
+}
+
+#[derive(Debug)]
+pub struct Cfg {
+    pub aiven_api_token: String,
+    pub billing_group_id: String,
+    pub bigquery_project_id: String,
+    pub bigquery_dataset: String,
+    pub bigquery_table: String,
+}
+
+impl Cfg {
+    fn new() -> Result<Self> {
+        Ok(Self {
+            // Aiven stuff
+            aiven_api_token: std::env::var("AIVEN_API_TOKEN").expect("api token"),
+            billing_group_id: "7d14362d-1e2a-4864-b408-1cc631bc4fab".to_owned(),
+            // BQ stuff
+            bigquery_project_id: "nais-io".to_owned(),
+            bigquery_dataset: "kafka_team_cost".to_owned(),
+            bigquery_table: "kafka_team_cost".to_owned(),
+        })
+}
+
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    init_tracing_subscriber()?;
+    info!("started kafka-cost");
+    let cfg = Cfg::new()?;
+    let aiven_client = client(&cfg.aiven_api_token)?;
+    let res = aiven_client.get(&format!("https://api.aiven.io/v1/billing-group/{}/invoice", cfg.billing_group_id)).bearer_auth(cfg.aiven_api_token).send().await?;
+    dbg!(res);
+    Ok(())
 }
