afd88ef71e3685a80d7a653b65f24d0d13c22b8f
carl hedgren <carl.hedgren@nav.no>
2025-05-21 10:56:35 +0200

    Scaffold kafka-kost
    Co-authored-by: x10an14-nav <christian.chavez@nav.no>

--- flake.nix
+++ flake.nix
@@ -1,26 +1,54 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.treefmt-nix = {
-    url = "github:numtide/treefmt-nix";
-    inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs =
-    inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, crane, flake-utils, advisory-db, ... }@inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import inputs.nixpkgs { localSystem = { inherit system; }; };
-      in
-      {
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource ./kafka-cost;
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = [ ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        kafka-cost =
+          craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+
+      in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ go ];
+          packages = with pkgs; [ delve go ginkgo postgresql rust-analyzer ];
+          inputsFrom = [ kafka-cost ];
         };
         formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
           programs.nixfmt.enable = true;
           programs.gofumpt.enable = true;
         };
-      }
-    );
+        packages = { default = kafka-cost; };
+
+      });
 }
